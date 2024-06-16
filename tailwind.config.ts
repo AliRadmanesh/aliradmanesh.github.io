6f645a9d64885f0cb0ae1a4d251b00d9b1576251
Ali Radmanesh <ali.radmanesh1995@gmail.com>
2024-06-16 09:02:42 +0330

    add tailwindcss base configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        primary: "#F8D06B",
+        black: "#333333",
+      }
     },
   },
   plugins: [],
 };
+
 export default config;
